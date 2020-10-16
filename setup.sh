cea6556544767390685986815d4a973b50d0835b
Nikita Naumenko <naumenkoniki@gmail.com>
2020-10-16 15:48:03 +0300

    Upgrade setup

--- setup.sh
+++ setup.sh
@@ -2,17 +2,20 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # Setup ASDF
+echo -e "\n--------------Setup ASDF--------------\n"
 brew install asdf
 
 echo -e "\n. $(brew --prefix asdf)/asdf.sh" >> ~/.zshrc
 echo -e "\n. $(brew --prefix asdf)/etc/bash_completion.d/asdf.bash" >> ~/.zshrc
 
+echo -e "\n--------------Setup Utils--------------\n"
 # Setup utils
 brew install \
   coreutils automake autoconf openssl \
   libyaml readline libxslt libtool unixodbc \
   unzip curl gpg git bat htop fzf diff-so-fancy
 
+echo -e "\n--------------Configure Git--------------\n"
 git config --global user.name "Nikita Naumenko"
 git config --global user.email naumenkoniki@gmail.com
 git config --global core.editor nvim
@@ -20,8 +23,10 @@ git config --global core.editor nvim
 git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
 $(brew --prefix)/opt/fzf/install
 
+echo -e "\n--------------Create SSH--------------\n"
 ssh-keygen -t rsa -b 4096 -C "naumenkoniki@gmail.com"
 
+echo -e "\n--------------Setup Languages--------------\n"
 # Setup languages
 asdf plugin-add ruby
 asdf install ruby latest
@@ -51,6 +56,7 @@ asdf install nodejs latest
 # Install GithubCLI
 brew install gh
 
+echo -e "\n--------------Setup Neovim--------------\n"
 # Install Neovim
 brew install neovim
 
