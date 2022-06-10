c423d580b39cac148a5fea3718ddea057359a37d
Nikita Naumenko <naumenkoniki@icloud.com>
2022-06-10 19:29:05 +0300

    fix treesiter

--- files/vim/lua/plugins/treesitter.lua
+++ files/vim/lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return function()
     require"nvim-treesitter.configs".setup {
-        ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+        ensure_installed = { "lua", "ruby", "javascript", "json", "html", "go", "dockerfile", "elixir", "cmake", "yaml", },
         highlight = {
             enable = true, -- false will disable the whole extension
         },
