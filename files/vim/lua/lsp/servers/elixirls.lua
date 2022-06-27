3bc56867b4c8456bd6942029d6e9f93cc7bc37ee
Nikita Naumenko <naumenkoniki@icloud.com>
2022-06-27 18:45:13 +0300

    fix elixir ls

--- files/vim/lua/lsp/servers/elixirls.lua
+++ files/vim/lua/lsp/servers/elixirls.lua
@@ -1,7 +1,3 @@
 return function(config, on_attach)
-    config.elixirls.setup {
-        on_attach = on_attach,
-        flags = {debounce_text_changes = 150}
-    }
+    return {on_attach = on_attach, flags = {debounce_text_changes = 150}}
 end
-
