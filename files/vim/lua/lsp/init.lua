7218dfb12615b60a31d952275af54fa16863208a
Nikita Naumenko <naumenkoniki@gmail.com>
2022-12-04 19:50:38 +0500

    fix prettier

--- files/vim/lua/lsp/init.lua
+++ files/vim/lua/lsp/init.lua
@@ -40,7 +40,7 @@ null_ls.setup({
   sources = {
         null_ls.builtins.diagnostics.eslint,
         null_ls.builtins.code_actions.eslint,
-        null_ls.builtins.formatting.prettier,
+        -- null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.fixjson,
         null_ls.builtins.formatting.lua_format,
         null_ls.builtins.diagnostics.credo
