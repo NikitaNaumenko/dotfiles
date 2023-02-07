81352b9644c5907efd37492b7bebe0bd13bb0dfb
Nikita Naumenko <naumenkoniki@gmail.com>
2023-02-07 23:46:10 +0500

    use nord

--- files/vim/lua/settings.lua
+++ files/vim/lua/settings.lua
@@ -1,1 +1,50 @@
-require("settings.init")
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.o.shortmess = vim.o.shortmess .. "c"
+vim.o.completeopt = "menuone,noselect"
+vim.bo.matchpairs = "(:),{:},[:],<:>"
+
+vim.api.nvim_command('colorscheme nord')
+-- more info :h vim.o
+local editor_options = {
+    compatible = false,
+    background = "dark",
+
+    encoding = "UTF-8",
+    fileencoding = "UTF-8",
+    langmenu = "en_US.UTF-8",
+
+    backup = false,
+    swapfile = false,
+    autoread = true,
+    autowrite = true,
+    writebackup = false,
+    history = 50,
+    laststatus = 2,
+    shell = os.getenv("SHELL"),
+    tabstop = 2,
+    expandtab = true,
+    shiftwidth = 2,
+    showbreak = "↪| ",
+    hlsearch = true,
+    incsearch = true,
+    ignorecase = true,
+    showmode = false,
+    linespace = 2,
+    termguicolors = true,
+    cmdheight = 2,
+    updatetime = 100,
+    timeoutlen = 500
+}
+for key, val in pairs(editor_options) do vim.o[key] = val end
+
+local window_options = {
+    number = true,
+    cursorline = true,
+    wrap = true,
+    list = true
+}
+for key, val in pairs(window_options) do vim.wo[key] = val end
+
+-- Plugins --
+vim.g["test#strategy"] = "toggleterm"
